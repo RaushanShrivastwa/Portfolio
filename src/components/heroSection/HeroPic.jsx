bb136b125a9161d624e267e39fcfd415c4a923ed
Raushan Shrivastwa <raushan.23bce7219@vitapstudent.ac.in>
2025-07-27 01:22:41 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/me.png"
+        src="https://freeimage.host/i/me.FvK6HRS"
         alt="Raushan Shrivastwa"
         className="max-h-[450px] w-auto"
       />
