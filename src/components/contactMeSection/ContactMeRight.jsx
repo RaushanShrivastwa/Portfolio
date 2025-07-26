262079c16230c60ef0c4d5d332d1dc0401cd33cd
Raushan Shrivastwa <raushan.23bce7219@vitapstudent.ac.in>
2025-07-27 01:24:06 +0530

    Update ContactMeRight.jsx

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -5,7 +5,7 @@ const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
       <img
-        src="../../public/images/email-image.png"
+        src="https://freeimage.host/i/email-image.FvKtoG9"
         alt="email image"
         className="max-w-[300px]"
       />
